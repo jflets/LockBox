d2a734dd7cc5c9aab5658a944411fe4b6ac7a0c8
jflets <jfletorides02@gmail.com>
2023-07-08 21:13:09 +0100

    Add random password generator

--- run.py
+++ run.py
@@ -72,3 +72,7 @@ def get_password_from_user():
             sys.stdout.write("*")
             sys.stdout.flush()
     return password
+
+def generate_random_password(length=12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    return "".join(random.choice(characters) for _ in range(length))
